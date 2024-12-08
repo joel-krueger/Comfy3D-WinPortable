f7e20240baba1ba92f1daa8384aed8d10bb172ff
YAN Wenkun <code@yanwk.fun>
2024-12-09 05:23:46 +0800

    compile-install pointnet2_ops

--- pt24-cu121/attachments/!FIRST_RUN-compile-install.bat
+++ pt24-cu121/attachments/!FIRST_RUN-compile-install.bat
@@ -1,3 +1,7 @@
+@REM Edit this to your GPU arch.
+@REM The "+PTX" here is a fail-safe (forward compatibility), you don't need to add that.
+set TORCH_CUDA_ARCH_LIST=6.1+PTX
+
 @REM Compile-install PyTorch3D
 @REM PyTorch3D on Windows works best through compile-install. Binary-install will fail on some workflows.
 @REM e.g. "CUDA error: no kernel image is available for execution on the device"
@@ -7,3 +11,11 @@ set PATH=%PATH%;%~dp0\python_embeded\Scripts
 
 .\python_embeded\python.exe -s -m pip install --force-reinstall ^
  "git+https://github.com/facebookresearch/pytorch3d.git@stable"
+
+@REM Compile-install pointnet2_ops for Triplane Gaussian Transformers
+
+git clone --depth=1 https://github.com/MrForExample/Comfy3D_Pre_Builds.git ^
+ .\tmp_build\Comfy3D_Pre_Builds
+
+.\python_embeded\python.exe -s -m pip install --force-reinstall ^
+ .\tmp_build\Comfy3D_Pre_Builds\_Libs\pointnet2_ops
--- pt24-cu121/attachments/中文脚本/!首次运行-编译安装依赖项.bat
+++ pt24-cu121/attachments/中文脚本/!首次运行-编译安装依赖项.bat
@@ -1,3 +1,7 @@
+@REM 编辑为你的 GPU 对应架构
+@REM 修改时无需保留 "+PTX" ，其用于向前兼容，防止用户忘了该步骤。
+set TORCH_CUDA_ARCH_LIST=6.1+PTX
+
 @REM 编译安装 PyTorch3D
 @REM PyTorch3D 对 Windows 不甚友好，所有二进制安装都可能在某个节点报错，极难排查，故最好提前编译安装。
 
@@ -8,3 +12,11 @@ set PIP_INDEX_URL=https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple
 
 .\python_embeded\python.exe -s -m pip install --force-reinstall ^
  "git+https://ghp.ci/https://github.com/facebookresearch/pytorch3d.git@stable"
+
+@REM 编译安装 pointnet2_ops ，该组件用于 Triplane Gaussian Transformers
+
+git clone --depth=1 https://ghp.ci/https://github.com/MrForExample/Comfy3D_Pre_Builds.git ^
+ .\tmp_build\Comfy3D_Pre_Builds
+
+.\python_embeded\python.exe -s -m pip install --force-reinstall ^
+ .\tmp_build\Comfy3D_Pre_Builds\_Libs\pointnet2_ops
