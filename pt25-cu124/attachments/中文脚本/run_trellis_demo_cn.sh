ca76d7a26257841ae3e2013049938264b3880403
YAN Wenkun <code@yanwk.fun>
2024-12-18 15:26:04 +0800

    NO_PROXY env var incompatible

--- pt25-cu124/attachments/中文脚本/run_trellis_demo_cn.sh
+++ pt25-cu124/attachments/中文脚本/run_trellis_demo_cn.sh
@@ -29,14 +29,6 @@ export ATTN_BACKEND="xformers"
 #export HTTPS_PROXY=$HTTP_PROXY
 #export http_proxy=$HTTP_PROXY
 #export https_proxy=$HTTP_PROXY
-#export NO_PROXY="localhost,*.local,*.internal,[::1],fd00::/7,
-#10.0.0.0/8,127.0.0.0/8,169.254.0.0/16,172.16.0.0/12,192.168.0.0/16,
-#10.*,127.*,169.254.*,172.16.*,172.17.*,172.18.*,172.19.*,172.20.*,
-#172.21.*,172.22.*,172.23.*,172.24.*,172.25.*,172.26.*,172.27.*,
-#172.28.*,172.29.*,172.30.*,172.31.*,172.32.*,192.168.*,
-#*.cn,ghproxy.com,*.ghproxy.com,ghproxy.org,*.ghproxy.org,
-#gh-proxy.com,*.gh-proxy.com,ghproxy.net,*.ghproxy.net"
-#export no_proxy=$NO_PROXY
 #echo "[INFO] Proxy set to $HTTP_PROXY"
 
 # 配置使用国内镜像站点
