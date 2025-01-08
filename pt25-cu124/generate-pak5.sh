2bdb503a907b164fc875b37c48b1eb98d889370a
YAN Wenkun <code@yanwk.fun>
2025-01-08 20:42:22 +0800

    Update deps for Manager

--- pt25-cu124/generate-pak5.sh
+++ pt25-cu124/generate-pak5.sh
@@ -6,14 +6,14 @@ echo '#' > pak5.txt
 array=(
 https://github.com/comfyanonymous/ComfyUI/raw/refs/heads/master/requirements.txt
 https://github.com/MrForExample/ComfyUI-3D-Pack/raw/a35a737676cf3cbb23360d98032870e242dae199/requirements.txt
+https://github.com/edenartlab/eden_comfy_pipelines/raw/refs/heads/main/requirements.txt
 https://github.com/kijai/ComfyUI-KJNodes/raw/refs/heads/main/requirements.txt
 https://github.com/Kosinkadink/ComfyUI-VideoHelperSuite/raw/refs/heads/main/requirements.txt
 https://github.com/ltdrdata/ComfyUI-Impact-Pack/raw/refs/heads/Main/requirements.txt
 https://github.com/ltdrdata/ComfyUI-Impact-Subpack/raw/refs/heads/main/requirements.txt
 https://github.com/ltdrdata/ComfyUI-Inspire-Pack/raw/refs/heads/main/requirements.txt
-https://github.com/WASasquatch/was-node-suite-comfyui/raw/refs/heads/main/requirements.txt
-https://github.com/edenartlab/eden_comfy_pipelines/raw/refs/heads/main/requirements.txt
 https://github.com/ltdrdata/ComfyUI-Manager/raw/refs/heads/main/requirements.txt
+https://github.com/WASasquatch/was-node-suite-comfyui/raw/refs/heads/main/requirements.txt
 )
 
 for line in "${array[@]}";
